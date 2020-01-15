4cd897bc65d59d25a0f0b52524721b9cba7db713
Suthep Chanchuphol <moo_suthep@hotmail.com>
2020-01-15 20:56:53 +0700

    > Move frontend files

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,23 +1,28 @@
-$(document).ready(function(){
-    let mainNav = $('#js-menu')
-    let navBarToggle = $('#js-navbar-toggle')
+$(document).ready(function() {
+  let mainNav = $("#js-menu");
+  let navBarToggle = $("#js-navbar-toggle");
 
-    navBarToggle.click( () => {
-        mainNav.toggleClass("active");
-    })
-  
-    $('#home').click( () => {
-        $('html, body').animate({
-            scrollTop: $("#intro").offset().top - 100
-        }, 2000);
-    })
-    window.onscroll = function() {scrollFunction()};
-
-    // function scrollFunction() {
-    //     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    //         document.getElementById("navbar").style.top = "0";
-    //     } else {
-    //         document.getElementById("navbar").style.top = "-73px";
-    //     }
-    // }
+  navBarToggle.click(() => {
+    mainNav.toggleClass("active");
   });
+
+  $("#home").click(() => {
+    $("html, body").animate(
+      {
+        scrollTop: $("#intro").offset().top - 100
+      },
+      2000
+    );
+  });
+  window.onscroll = function() {
+    scrollFunction();
+  };
+
+  // function scrollFunction() {
+  //     if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+  //         document.getElementById("navbar").style.top = "0";
+  //     } else {
+  //         document.getElementById("navbar").style.top = "-73px";
+  //     }
+  // }
+});
