ddeb514d56413e487e5ba76f2c9fc4bb92939903
thekogo <thekogo@gmail.com>
2020-01-20 18:36:08 +0700

    add timout session

--- backend/config/auth.js
+++ backend/config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
     facebook: {
         APP_ID: "838390816607063",
         APP_SECRET: "3760db2dd810bed4079655effb99b307",
-        callbackURL: "http://localhost:5000/auth/facebook/callback"
+        callbackURL: "http://localhost:3000/auth/facebook/callback"
     }
 }
