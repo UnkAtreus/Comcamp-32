7aa8f897b0c2bcd54c5c5e2240ef143e541456c2
Kittipat Dechkul <unk.atreus@gmail.com>
2020-01-26 16:26:00 +0700

    Add Faq

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -69,3 +69,12 @@ $(document).ready(function() {
         speedAsDuration: true
     });
 });
+
+const items = document.querySelectorAll(".accordion a");
+
+function toggleAccordion() {
+    this.classList.toggle('active');
+    this.nextElementSibling.classList.toggle('active');
+}
+
+items.forEach(item => item.addEventListener('click', toggleAccordion));
