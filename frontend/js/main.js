dd4a3340922104041c54d37cb8586f29f417b6e4
Suthep Chanchuphol <moo_suthep@hotmail.com>
2020-01-31 03:48:28 +0700

    ^ Optimizing things
    - ลำดับการโหลดไฟล์ต่างๆ
    - ทำ Lazy load images
    - แยก `main.js` ออกเป็นตัวสำคัญกับไม่สำคัญ เพื่ออย่างน้อยให้เกิด first paint เร็วขึ้น
    - ปรับ accessibility
      - ใส่ `aria-label` ให้ปุ่ม
      - ใส่ `alt` ให้กับรูปทุกรูป
      - แก้ contrast font/bg
      - ทำ `font-display: swap`
    - Stats from Lighthouse:
      - Mobile -> 14/77/86/91 to 33/100/86/100
      - Desktop -> 28/78/86/89 to 35/92/86/100

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,91 +1,71 @@
-/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-particlesJS.load('particles-js', 'js/particlesjs-config.json', function() {});
-$(document).ready(function() {
-    $('.owl-carousel').owlCarousel({
-        margin: 10,
-        nav: true,
-        navText: [
-            '<div class="Subject-Arrow-Left-Column"><div class="Subject-Left-SpriteMaterial"></div></div>',
-            '<div class="Subject-Arrow-Right-Column"><div class="Subject-Right-SpriteMaterial"></div></div>'
-        ],
-        navContainerClass: 'Subject-Slider-Control',
-        navClass: [
-            'slider-control-centerleft',
-            'slider-control-centerright'
-        ],
-        navElement: 'div',
-        stageOuterClass: 'Subject-Stage-Outer',
-        stageClass: 'Subject-Stage',
-        dotsContainer: '#carousel-custom-dots',
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 1
-            },
-            1000: {
-                items: 1
-            }
-        },
-        onChanged: function(event) {
-            var item = event.item.index;
-            var actives = document.getElementsByClassName("Subject-Carousel-Pager");
-            $(actives[item]).addClass('active').siblings().removeClass('active');
+// เอา config ที่ไม่ใช่เห็นทันทีไว้ในนี้ เพื่อลดเวลารัน
 
-        }
-
-    });
-    $('.Subject-Carousel-Pager').click(function() {
-        $('.owl-carousel').trigger('to.owl.carousel', [$(this).index(), 300]);
-        $(this).addClass('active').siblings().removeClass('active');
-
-    });
-    $('.Gellary').owlCarousel({
-        stageOuterClass: 'About-Pic-Column',
-        stageClass: 'About-Pic',
-        autoplay: true,
-        nav: true,
-        navContainerClass: 'Subject-Slider-Control',
-        navClass: [
-            'slider-control-centerleft',
-            'slider-control-centerright'
-        ],
-        dotsClass: 'About-Dots-Container',
-        responsive: {
-            0: {
-                items: 1
-            },
-            600: {
-                items: 1
-            },
-            1000: {
-                items: 1
-            }
-        }
-    });
-    var scroll = new SmoothScroll('a[href*="#"]', {
-        speed: 300,
-        speedAsDuration: true
-    });
+// Carousel
+$(".owl-carousel").owlCarousel({
+  margin: 10,
+  nav: true,
+  navText: [
+    '<div class="Subject-Arrow-Left-Column"><div class="Subject-Left-SpriteMaterial"></div></div>',
+    '<div class="Subject-Arrow-Right-Column"><div class="Subject-Right-SpriteMaterial"></div></div>'
+  ],
+  navContainerClass: "Subject-Slider-Control",
+  navClass: ["slider-control-centerleft", "slider-control-centerright"],
+  navElement: "div",
+  stageOuterClass: "Subject-Stage-Outer",
+  stageClass: "Subject-Stage",
+  dotsContainer: "#carousel-custom-dots",
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 1
+    }
+  },
+  onChanged: function(event) {
+    var item = event.item.index;
+    var actives = document.getElementsByClassName("Subject-Carousel-Pager");
+    $(actives[item])
+      .addClass("active")
+      .siblings()
+      .removeClass("active");
+  }
+});
+$(".Subject-Carousel-Pager").click(function() {
+  $(".owl-carousel").trigger("to.owl.carousel", [$(this).index(), 300]);
+  $(this)
+    .addClass("active")
+    .siblings()
+    .removeClass("active");
+});
+$(".Gellary").owlCarousel({
+  stageOuterClass: "About-Pic-Column",
+  stageClass: "About-Pic",
+  autoplay: true,
+  nav: true,
+  navContainerClass: "Subject-Slider-Control",
+  navClass: ["slider-control-centerleft", "slider-control-centerright"],
+  dotsClass: "About-Dots-Container",
+  responsive: {
+    0: {
+      items: 1
+    },
+    600: {
+      items: 1
+    },
+    1000: {
+      items: 1
+    }
+  }
 });
 
-const items = document.querySelectorAll(".accordion a");
-
+// Accordion
+var items = document.querySelectorAll(".accordion a");
 function toggleAccordion() {
-    this.classList.toggle('active');
-    this.nextElementSibling.classList.toggle('active');
+  this.classList.toggle("active");
+  this.nextElementSibling.classList.toggle("active");
 }
-
-items.forEach(item => item.addEventListener('click', toggleAccordion));
-
-var navState = 0;
-var navElement = document.getElementById("Nav-mobile");
-function toggleMobileNav(){
-    if (navState) {
-        navElement.classList.remove("Nvmb-active")
-    } else {
-        navElement.classList.add("Nvmb-active")
-    }
-    navState = !navState;
-}
+items.forEach(item => item.addEventListener("click", toggleAccordion));
