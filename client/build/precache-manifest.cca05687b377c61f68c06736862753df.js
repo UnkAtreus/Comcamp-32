bde56c571bb1c24c9aafff59cf54f663166fcc4a
thekogo <thekogo@gmail.com>
2020-02-05 16:01:30 +0700

    add build

---
+++ client/build/precache-manifest.cca05687b377c61f68c06736862753df.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8435aa1a8f26f4597d0ccdd9fc61a418",
+    "url": "/index.html"
+  },
+  {
+    "revision": "76ad8d7167d8544dfef2",
+    "url": "/static/css/2.1e105a3b.chunk.css"
+  },
+  {
+    "revision": "46228cf4e7cb7536b1d8",
+    "url": "/static/css/main.da850eda.chunk.css"
+  },
+  {
+    "revision": "76ad8d7167d8544dfef2",
+    "url": "/static/js/2.97026d11.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.97026d11.chunk.js.LICENSE"
+  },
+  {
+    "revision": "46228cf4e7cb7536b1d8",
+    "url": "/static/js/main.765cf437.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "62baf0f264db531a9457567a97c2311d",
+    "url": "/static/media/Button_Frame.62baf0f2.png"
+  },
+  {
+    "revision": "a4c5e1e966947ea2a3c4c07a1f3b0650",
+    "url": "/static/media/CC_Button_middle.a4c5e1e9.png"
+  },
+  {
+    "revision": "fd017ec237c384ba89b4b46d49c59dc5",
+    "url": "/static/media/General_BG_new.fd017ec2.png"
+  },
+  {
+    "revision": "028337253a13eb43df278ec7aa660037",
+    "url": "/static/media/logo-big.02833725.png"
+  }
+]);
