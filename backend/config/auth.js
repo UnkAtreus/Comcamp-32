44d6014bb6970b571fd1292f2236c104b3fcf206
yee2542 <yee2542gtl@gmail.com>
2020-02-06 21:26:20 +0700

    update new api facebook dev

--- backend/config/auth.js
+++ backend/config/auth.js
@@ -1,9 +1,9 @@
 module.exports = {
     facebook: {
         // APP_ID: "838390816607063",
-        APP_ID: "1963740163874549",
+        APP_ID: "183393066216429",
         // APP_SECRET: "3760db2dd810bed4079655effb99b307",
-        APP_SECRET: "dce299b451abbb9d8f48dc43548976e1",
+        APP_SECRET: "0f237410d6e0f3d8fa830f32e4e426b9",
         callbackURL: `http://${process.env.HOST}/auth/facebook/callback`
     }
 }
