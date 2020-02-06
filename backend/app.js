5c4a40ebbee5b53aaf0f2bbf92353c5223aa519b
yee2542 <yee2542gtl@gmail.com>
2020-02-06 16:34:51 +0700

    apply cors & mongodb setting

--- backend/app.js
+++ backend/app.js
@@ -17,29 +17,44 @@ var bodyParser = require('body-parser');
 const cors = require('cors')
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 
 /***************Mongodb configuratrion********************/
 var mongoose = require('mongoose');
 var configDB = require('./config/database.js');
 //configuration ===============================================================
-mongoose.connect(configDB.url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, () => {
-    console.log("Connect Database")
-}); // connect to our database
+// mongoose.connect(configDB.url, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false }, () => {
+//     console.log("Connect Database")
+// }); // connect to our database
+mongoose.connect(
+    'mongodb://mongocc32:27017/comcamp', {
+    // 'mongodb://localhost:27017/comcamp', {
+    user: 'root',
+    pass: 'comcamp32dbeuei',
+    authSource: 'admin',
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+}, (err) => console.log('mongodb err', err))
 
 
 require('./app/passport')(passport); // pass passport for configuration
 
-app.use(cors())
+app.use(cors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+}))
 
 //session required for passport
-app.use(session({ 
+app.use(session({
     // store: new RedisStore({ host: 'localhost', port: 6379, client: redisClient}),
     secret: 'WeLoveComcamp32EiEi',
     resave: false,
     saveUninitialized: true,
-    cookie: {maxAge: 30 * 60 * 1000}
+    cookie: { maxAge: 30 * 60 * 1000 }
 }));
 
 app.use(passport.initialize());
