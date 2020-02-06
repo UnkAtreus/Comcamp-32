a7ce053fe155403372b4823a3b7a146fb9d4dbfa
yee2542 <yee2542gtl@gmail.com>
2020-02-07 01:39:13 +0700

    change app id

--- backend/config/auth.js
+++ backend/config/auth.js
@@ -1,9 +1,10 @@
 module.exports = {
     facebook: {
         // APP_ID: "838390816607063",
-        APP_ID: "183393066216429",
+        APP_ID: "838390816607063",
         // APP_SECRET: "3760db2dd810bed4079655effb99b307",
-        APP_SECRET: "0f237410d6e0f3d8fa830f32e4e426b9",
-        callbackURL: `http://${process.env.HOST}/auth/facebook/callback`
+        APP_SECRET: "3760db2dd810bed4079655effb99b307",
+        // callbackURL: `http://${process.env.HOST}/auth/facebook/callback`
+        callbackURL: `https://${process.env.HOST}/auth/facebook/callback`
     }
 }
