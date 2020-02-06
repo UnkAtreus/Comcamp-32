31b4557b086f773b116f5490d7dd7e015a4faa63
yee2542 <yee2542gtl@gmail.com>
2020-02-06 17:23:49 +0700

    fix geenral issues

--- backend/config/auth.js
+++ backend/config/auth.js
@@ -3,6 +3,7 @@ module.exports = {
         APP_ID: "838390816607063",
         APP_SECRET: "3760db2dd810bed4079655effb99b307",
         // callbackURL: "http://localhost:3000/auth/facebook/callback"
-        callbackURL: "http://localhost:5000/auth/facebook/callback"
+        // callbackURL: "http://localhost:5000/auth/facebook/callback"
+        callbackURL: "/auth/facebook/callback"
     }
 }
